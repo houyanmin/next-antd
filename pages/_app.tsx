06ae92d7a060e934d14f7e06d6759053d7568c57
houyanmin <houyanmin923@gmail.com>
2021-04-08 20:26:35 +0800

    清除代码

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import { useHistory, useLocation } from "react-router-dom";
+import React, { useEffect } from 'react'
 import { NextComponentType } from 'next'
 import { AppContext, AppProps } from 'next/app'
 import { ConfigProvider } from 'antd'
-import { Provider } from 'react-redux'
-import { useStore } from '../store/store'
 import "../styles/globals.less";
 import 'antd/dist/antd.less'
 import zhCN from 'antd/lib/locale/zh_CN';
-import HeaderComp from "../components/Layout/header";
-import FooterComp from "../components/Layout/footer";
 import "./style.less";
 
 type TAppProps = {
-  a: string
-  initial_redux_state?: any
+  initial?: any
 }
 type TProps = {
   [key in string]: string
@@ -32,61 +26,16 @@ const AppCom: NextComponentType<
   ModifiedAppInitialProps<TAppProps>,
   ExtendedAppProps<TProps, TAppProps>
 > = ({ Component, appProps }) => {
-  // redux store init state
-  const store = useStore(appProps.initial_redux_state)
-  // const history = useHistory();
-  // const location = useLocation();
-  // const { userStore } = useStore(appProps.initial_redux_state);
-  // const {getUserInfo, loginout} = userStore;
 
-  const [isLogin, setIsLogin] = useState(false)
-  const [userInfo, serUserInfo] = useState(null)
 
   useEffect(() => {
-    // const token = Cookies.get('token');
-    // if(token) {
-    //   getUser()
-    // }
-    // emitEv.on('event-user-info', () => {
-    //   getUser()
-    // })
-    // emitEv.on('event-user-logout', async () => {
-    //   await loginout()
-    //   Cookies.remove('token', { path: '/',domain: process.env.REACT_APP_BASE_DOMAIN });
-    //   serUserInfo(null)
-    //   setIsLogin(false)
-    //   history.push('/')
-    // })
-    // return () => {
-    //   emitEv.removeAllListeners('event-user-info')
-    //   emitEv.removeAllListeners('event-user-logout')
-    // }
+
   },[])
 
-  // useEffect(() => {
-  //   document.documentElement.scrollTop = document.body.scrollTop =0;
-  // }, [location]);
-
-  let getUser = async() => {
-    // let info = await getUserInfo();
-    // if(info.mobile == '' || info.mobile == null || info.mobile == undefined){
-    //   emitEv.emit('event-mobile-modal')
-    // }
-    // setIsLogin(true)
-    // serUserInfo(info)
-  }
 
   return (
     <ConfigProvider locale={zhCN}>
-      <Provider store={store}>
-        <div className="warp-layout">
-          <HeaderComp isLogin={isLogin} {...userInfo}/>
-          <div className="warp-layout-content">
-              <Component {...appProps} />
-          </div>
-          <FooterComp />
-        </div>
-      </Provider>
+      <Component {...appProps} />
     </ConfigProvider>
   )
 }
@@ -96,10 +45,9 @@ async function appGetInitialProps(): Promise<
 > {
   return {
     appProps: {
-      initial_redux_state: {
+      initial: {
         user: { nextUserId: 1 }
-      },
-      a: '134123'
+      }
     }
   }
 }
